68f45f71d9329bc156fb0f1f9052bdaa8bcb580d
orieabynmlna <orieabyanm@gmail.com>
2025-09-03 11:33:49 +0700

    ASD/Bab1

---
+++ Algoritma_Struktur_Data/Array.java
@@ -0,0 +1,17 @@
+package Algoritma_Struktur_Data;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] angka = { 30, 87, 90, 3, 1, 50, 23, 4, 25, 23, 40, 35, 47, 2, 33};
+
+        // Mengakses elemen array
+        System.out.println("Elemen pertama: " + angka[0]);
+        System.out.println("Elemen kedua: " + angka[1]);
+
+        // Menampilkan semua elemen array
+        System.out.println("\nSemua elemen dalam array:");
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println("Elemen ke-" + (i + 1) + ": " + angka[i]);
+        }
+    }
+}
