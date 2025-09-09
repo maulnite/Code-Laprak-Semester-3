fd55b1d076e636f2e4f258634b6567d17eb604c8
orieabynmlna <orieabyanm@gmail.com>
2025-09-09 16:04:19 +0700

    LC

--- PRAKTIKUM_ASD/LiveCoding/Bab1/Soal2.java
+++ PRAKTIKUM_ASD/LiveCoding/Bab1/Soal2.java
@@ -5,35 +5,36 @@ import java.util.*;
 public class Soal2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int m = input.nextInt();
-        int n = input.nextInt();
-        int[] array = new int[m * n];
 
-        for (int i = 0; i < array.length; i++) {
+        int r = input.nextInt();
+        int c = input.nextInt();
+        int total = r * c;
+
+        int[] array = new int[total];
+        for (int i = 0; i < total; i++) {
             array[i] = input.nextInt();
         }
+
         int t = input.nextInt();
-        for (int i = 0; i < t; i++) {
-            int l = input.nextInt();
+        while (t-- > 0) {
+            int i = input.nextInt();
             int p = input.nextInt();
-            int temp = array[l];
-            if (l < p) {
-                for (int j = l; j < p; j++) {
-                    array[j] = array[j + 1];
-                }
-            } else if (l > p) {
-                for (int j = l; j > p; j--) {
-                    array[j] = array[j - 1];
-                }
+
+            int simpan = array[i];
+
+            if (i < p) {
+                System.arraycopy(array, i + 1, array, i, p - i);
+            } else if (i > p) {
+                System.arraycopy(array, p, array, p + 1, i - p);
             }
-            array[p] = temp;
+            array[p] = simpan;
         }
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(array[i * n + j] + " ");
+        for (int i = 0; i < total; i++) {
+            System.out.print(array[i] + " ");
+            if ((i + 1) % c == 0) {
+                System.out.println();
             }
-            System.out.println();
         }
 
         input.close();
